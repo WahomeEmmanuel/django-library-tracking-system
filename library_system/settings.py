5a946b7588aeb07db22ada61ac644c9d9dd868e8
Emmanuel Gathage <gathage.ew@gmail.com>
2026-03-02 16:40:04 +0300

    Add pagination

--- library_system/settings.py
+++ library_system/settings.py
@@ -103,7 +103,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 # Celery Configuration
@@ -115,7 +117,7 @@ CELERY_RESULT_SERIALIZER = 'json'
 
 CELERY_BEAT_SCHEDULE = {
     'check_overdue_loans': {
-        
+
     }
 }
 
